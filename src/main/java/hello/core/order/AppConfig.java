cc1ae31b54ac056b97b08106f5917af8aafa6bc9
Euiseon <inia8inia@gmail.com>
2022-11-02 22:23:17 +0900

    3-3. AppConfig 리팩터링 - 역할과 구현 분리

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -1,17 +1,33 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
 public class AppConfig {
+    
+    //new MemoryMemberRepository 중복 제거 + 역할과 구현클래스를 구분
 
+    //MemberService 역할
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    //MemberRepository 역할
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    //OrderService 역할
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //DiscountPolicy 역할 -> FixDiscountPolicy를 지정
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
