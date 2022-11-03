edff8d2d7b547b622b6a3429317ecb1a2cb01db8
Euiseon <inia8inia@gmail.com>
2022-11-03 22:06:07 +0900

    3-4. AppConfig - 정률할인 선택 시 코드 변화

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -28,6 +29,7 @@ public class AppConfig {
 
     //DiscountPolicy 역할 -> FixDiscountPolicy를 지정
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
