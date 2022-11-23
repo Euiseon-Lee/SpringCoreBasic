800a3e6ef65fbd763da13a9ccfd992973072c7fb
Euiseon <inia8inia@gmail.com>
2022-11-23 21:52:45 +0900

    6-1. 컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     //구현체 하나만 있는 경우 인터페이스명에 Impl라고 관례상 붙임
     /*
@@ -20,6 +24,7 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
     //생성자를 통해서 MemberRepository를 가져옴 (생성자 주입)
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,20 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import static org.assertj.core.api.Assertions.*;
+public class AutoAppConfigTest {
+    @Test
+    void basicScan() {
+        //AnnotationConfigApplicationContext 를 사용하는 것은 기존과 동일하다.
+        //설정 정보로 AutoAppConfig 클래스를 넘겨준다.
+        //실행해보면 기존과 같이 잘 동작하는 것을 확인할 수 있다
+        ApplicationContext ac = new
+                AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
